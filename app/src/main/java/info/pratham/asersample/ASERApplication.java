344be9364ebc0b657e9cb9eeed6b5f605d5c2c72
prathamApp <prathambaner123@gmail.com>
2018-11-24 11:49:52 +0530

    pk commits

---
+++ app/src/main/java/info/pratham/asersample/ASERApplication.java
@@ -0,0 +1,52 @@
+package info.pratham.asersample;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.Random;
+
+/**
+ * Created by Pravin on 2 Feb 2018.
+ */
+
+public class ASERApplication extends Application {
+
+    public static ASERApplication aserApplication;
+    private static final DateFormat dateTimeFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.ENGLISH);
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        aserApplication = this;
+    }
+
+
+    public static String getVersion() {
+        Context context = ASERApplication.aserApplication;
+        String packageName = context.getPackageName();
+        try {
+            PackageManager pm = context.getPackageManager();
+            return pm.getPackageInfo(packageName, 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            return null;
+        }
+    }
+
+    public static ASERApplication getInstance() {
+        return aserApplication;
+    }
+
+    public static int getRandomNumber(int min, int max) {
+        return min + (new Random().nextInt(max));
+    }
+
+    public static String getCurrentDate() {
+        Calendar cal = Calendar.getInstance();
+        return dateTimeFormat.format(cal.getTime());
+    }
+}
---
+++ app/src/main/java/info/pratham/asersample/BaseActivity.java
@@ -0,0 +1,42 @@
+package info.pratham.asersample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Pravin on 19/03/2018.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
