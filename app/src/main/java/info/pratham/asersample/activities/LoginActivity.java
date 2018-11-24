829b36bfe4f9c6e164640ef5b2a29ce19cb362ef
prathamApp <prathambaner123@gmail.com>
2018-11-24 11:51:39 +0530

    pk commits

--- app/src/main/java/info/pratham/asersample/activities/LoginActivity.java
+++ app/src/main/java/info/pratham/asersample/activities/LoginActivity.java
@@ -1,15 +1,15 @@
 package info.pratham.asersample.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import info.pratham.asersample.BaseActivity;
 import info.pratham.asersample.R;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
