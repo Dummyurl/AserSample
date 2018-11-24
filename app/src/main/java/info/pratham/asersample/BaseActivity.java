a8899ba6a10f43a6b694d7c82ee84daf04e37ed1
prathamApp <prathambaner123@gmail.com>
2018-11-24 12:24:58 +0530

    pk commits

--- app/src/main/java/info/pratham/asersample/BaseActivity.java
+++ app/src/main/java/info/pratham/asersample/BaseActivity.java
@@ -1,8 +1,19 @@
 package info.pratham.asersample;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import info.pratham.asersample.utility.PermissionResult;
 
 /**
  * Created by Pravin on 19/03/2018.
@@ -10,33 +21,123 @@ import android.support.v7.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
 
+    private PermissionResult permissionResult;
+    private final int KEY_PERMISSION = 200;
+    private String permissionsAsk[];
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
+    public boolean isPermissionGranted(Context context, String permission) {
+        boolean granted = ((Build.VERSION.SDK_INT < Build.VERSION_CODES.M) || (ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED));
+        return granted;
+    }
+
+    /**
+     * @param context     current Context
+     * @param permissions String[] permission to ask
+     * @return boolean true/false
+     */
+    public boolean isPermissionsGranted(Context context, String permissions[]) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+            return true;
+
+        boolean granted = true;
+
+        for (String permission : permissions) {
+            if (!(ActivityCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED))
+                granted = false;
+        }
+
+        return granted;
+    }
+
+
+    private void internalRequestPermission(String[] permissionAsk) {
+        String arrayPermissionNotGranted[];
+        ArrayList<String> permissionsNotGranted = new ArrayList<>();
+
+        for (int i = 0; i < permissionAsk.length; i++) {
+            if (!isPermissionGranted(BaseActivity.this, permissionAsk[i])) {
+                permissionsNotGranted.add(permissionAsk[i]);
+            }
+        }
+
+
+        if (permissionsNotGranted.isEmpty()) {
+
+            if (permissionResult != null)
+                permissionResult.permissionGranted();
+
+        } else {
+
+            arrayPermissionNotGranted = new String[permissionsNotGranted.size()];
+            arrayPermissionNotGranted = permissionsNotGranted.toArray(arrayPermissionNotGranted);
+            ActivityCompat.requestPermissions(BaseActivity.this, arrayPermissionNotGranted, KEY_PERMISSION);
+
+        }
+
+
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        if (requestCode != KEY_PERMISSION) {
+            return;
+        }
+
+        List<String> permissionDenied = new LinkedList<>();
+        boolean granted = true;
+
+        for (int i = 0; i < grantResults.length; i++) {
+
+            if (!(grantResults[i] == PackageManager.PERMISSION_GRANTED)) {
+                granted = false;
+                permissionDenied.add(permissions[i]);
+            }
+
+        }
+
+        if (permissionResult != null) {
+            if (granted) {
+                permissionResult.permissionGranted();
+            } else {
+                for (String s : permissionDenied) {
+                    if (!ActivityCompat.shouldShowRequestPermissionRationale(this, s)) {
+                        permissionResult.permissionForeverDenied();
+                        return;
+                    }
+                }
+
+                permissionResult.permissionDenied();
+
+
+            }
+        }
+
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
+    /**
+     * @param permission       String permission ask
+     * @param permissionResult callback PermissionResult
+     */
+    public void askCompactPermission(String permission, PermissionResult permissionResult) {
+        permissionsAsk = new String[]{permission};
+        this.permissionResult = permissionResult;
+        internalRequestPermission(permissionsAsk);
+
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
+    /**
+     * @param permissions      String[] permissions ask
+     * @param permissionResult callback PermissionResult
+     */
+    public void askCompactPermissions(String permissions[], PermissionResult permissionResult) {
+        permissionsAsk = permissions;
+        this.permissionResult = permissionResult;
+        internalRequestPermission(permissionsAsk);
     }
 }
--- app/src/main/java/info/pratham/asersample/activities/LoginActivity.java
+++ app/src/main/java/info/pratham/asersample/activities/LoginActivity.java
@@ -1,15 +1,49 @@
 package info.pratham.asersample.activities;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import info.pratham.asersample.BaseActivity;
 import info.pratham.asersample.R;
+import info.pratham.asersample.utility.PermissionResult;
+import info.pratham.asersample.utility.PermissionUtils;
 
-public class LoginActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements PermissionResult {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        dynamicPermissionCheck();
+    }
+
+    private void dynamicPermissionCheck() {
+        if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)) {
+            //Dynamic permissions needed
+            String[] permissionArray = new String[]{PermissionUtils.Manifest_WRITE_EXTERNAL_STORAGE, PermissionUtils.Manifest_CAMERA};
+            if (!isPermissionsGranted(LoginActivity.this, permissionArray)) {
+                // permissions not granted
+                askCompactPermissions(permissionArray, this);
+            } else proceedFurther();
+        } else proceedFurther();
+    }
+
+    private void proceedFurther() {
+        // Application is ready to go with permission acceptance
+    }
+
+    @Override
+    public void permissionGranted() {
+
+    }
+
+    @Override
+    public void permissionDenied() {
+
+    }
+
+    @Override
+    public void permissionForeverDenied() {
+
     }
 }
