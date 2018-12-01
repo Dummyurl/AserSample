8582fbd7a9db07861d8e5c14cc9a0c56d8083469
prathamApp <prathambaner123@gmail.com>
2018-12-01 12:38:23 +0530

    pk

--- app/src/main/java/info/pratham/asersample/fragments/StudentDetails.java
+++ app/src/main/java/info/pratham/asersample/fragments/StudentDetails.java
@@ -21,6 +21,7 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import info.pratham.asersample.BaseFragment;
 import info.pratham.asersample.R;
+import info.pratham.asersample.activities.EnglishActivity;
 import info.pratham.asersample.activities.LanguageActivity;
 import info.pratham.asersample.database.modalClasses.Student;
 import info.pratham.asersample.utility.AserSampleUtility;
@@ -75,7 +76,7 @@ public class StudentDetails extends BaseFragment {
       //  if (!childFirstName.isEmpty() && !childFatherName.isEmpty() && !childVillageName.isEmpty() && selectedclass > 0 && agegroup != -1) {
 
           //  Student student = new Student(childFirstName, childFatherName, childVillageName, classChild.getSelectedItem().toString(), ((RadioButton) radioGroup.findViewById(agegroup)).getText().toString());
-            Intent intent = new Intent(getActivity(), LanguageActivity.class);
+            Intent intent = new Intent(getActivity(), EnglishActivity.class);
            // intent.putExtra("student", student);
             getActivity().startActivity(intent);
 
