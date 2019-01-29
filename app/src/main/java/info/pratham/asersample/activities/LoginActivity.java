e462d21905eb5a7b198de2ded6fc5fbb0865944d
prathamApp <prathambaner123@gmail.com>
2019-01-29 11:16:02 +0530

    Update LoginActivity.java

--- app/src/main/java/info/pratham/asersample/activities/LoginActivity.java
+++ app/src/main/java/info/pratham/asersample/activities/LoginActivity.java
@@ -47,7 +47,6 @@ public class LoginActivity extends BaseActivity implements PermissionResult {
         // Application is ready to go with permission acceptance
         //Hide notification bar
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        int a = 5/0;
         if (!createFolderStructureForStoringDataLocally())
             Toast.makeText(this, "Cannot create folder locally", Toast.LENGTH_SHORT).show();
         if (fragment == null) {
