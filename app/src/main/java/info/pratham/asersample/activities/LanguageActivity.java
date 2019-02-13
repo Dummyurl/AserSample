9dedaeca5c0069dbf11d5cd409ed6ef375aac227
prathamApp <prathamApp>
2019-02-13 15:07:35 +0530

    Amar

--- app/src/main/java/info/pratham/asersample/activities/LanguageActivity.java
+++ app/src/main/java/info/pratham/asersample/activities/LanguageActivity.java
@@ -228,6 +228,7 @@ public class LanguageActivity extends BaseActivity implements WordsListListener,
                     /*if (prevAttempted) {
                         break;
                     }*/
+
                 }
             }
             if (!prevAttempted && (currentLevel.equals(getString(R.string.Word)) || currentLevel.equals(getString(R.string.Letter)))) {
