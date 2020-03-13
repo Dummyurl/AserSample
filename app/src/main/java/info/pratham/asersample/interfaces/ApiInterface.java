e603fae8444cf99e56ed3d0d996abab9b8719213
prathamApp <prathamApp>
2020-03-13 11:32:12 +0530

    Android Pie network issue solved

--- app/src/main/java/info/pratham/asersample/interfaces/ApiInterface.java
+++ app/src/main/java/info/pratham/asersample/interfaces/ApiInterface.java
@@ -6,7 +6,6 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Header;
-import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface ApiInterface {
